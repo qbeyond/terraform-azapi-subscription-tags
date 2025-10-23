91975d85538f6acdd8cb9ee9ac5abcd37f6e827b
CaMMeu <145769396+CaMMeu@users.noreply.github.com>
2025-10-23 16:12:34 +0200

    updating azapi version to 2.7

--- main.tf
+++ main.tf
@@ -9,13 +9,11 @@ terraform {
 
 # The resource tags always exist. Therefore we need to use update resource
 resource "azapi_update_resource" "subscription_tags" {
-  type      = "Microsoft.Resources/tags@2021-04-01"
-  parent_id = "/subscriptions/${var.subscription_id}"
-  name      = "default"
+  type                    = "Microsoft.Resources/tags@2021-04-01"
+  parent_id               = "/subscriptions/${var.subscription_id}"
+  name                    = "default"
   ignore_missing_property = var.ignore_missing_property
-  body = jsonencode({
-    properties = {
-      tags = var.tags
-    }
-  })
+  body = {
+    tags = var.tags
+  }
 }
