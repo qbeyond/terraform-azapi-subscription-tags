459799c36487e1ae6e5bd3b6df35c15666651cde
CaMMeu <145769396+CaMMeu@users.noreply.github.com>
2026-04-17 13:27:49 +0200

    Updated Microsoft tags resources version to "Microsoft.Resources/tags@2025-04-01"

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -13,6 +13,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 - Using azapi_resource_action to fix the bug that it would only POST and not PUT. Now Tags that are not specified in the variable tags get deleted.
 
+### Changed
+
+- Updated Microsoft tags resources version to "Microsoft.Resources/tags@2025-04-01"
+
 ## [2.1.0] - 2025-10-23
 
 ### Changed
--- main.tf
+++ main.tf
@@ -9,13 +9,13 @@ terraform {
 
 
 data "azapi_resource_id" "tags" {
-  type      = "Microsoft.Resources/tags@2023-07-01"
+  type      = "Microsoft.Resources/tags@2025-04-01"
   parent_id = "/subscriptions/${var.subscription_id}"
   name      = "default"
 }
 # The resource tags always exist. Therefore we need to use resource action.
 resource "azapi_resource_action" "subscription_tags" {
-  type        = "Microsoft.Resources/tags@2023-07-01"
+  type        = "Microsoft.Resources/tags@2025-04-01"
   resource_id = data.azapi_resource_id.tags.id
   method      = "PUT"
   body = {
